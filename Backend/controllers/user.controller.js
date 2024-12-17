877ac48252bf1103106d5d44bbc31cf056230034
Praveen Kumar Prajapat <>
2024-12-17 12:54:31 +0530

    add profile and logout

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require('../model/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
-// const blackListTokenModel = require('../models/blackListToken.model');
+const blackListTokenModel = require('../model/blacklistToken.model');
 
 module.exports.registerUser = async (req, res, next) => {
 
@@ -31,4 +31,37 @@ module.exports.registerUser = async (req, res, next) => {
     res.status(201).json({ token, user });
 
 
+}
+
+
+module.exports.loginUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email }).select('password');
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid email or password' })
+    }
+
+    const isMatch = await user.comparePassword(password)
+
+    if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid email or password' })
+    }
+    const token = user.generateAuthToken();
+
+    res.status(200).json({ token, user });
+}
+
+
+module.exports.userProfile = (req, res, next) => {
+    res.status(200).json(req.user);
+}
+module.exports.logoutUser = async (req, res, next) => {
+    res.clearCookie('token')
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    await blackListTokenModel.create({ token })
+    res.status(200).json({ message: 'Logged out' });
 }
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require("express-validator")
 const userController = require('../controllers/user.controller');
-// const authMiddleware = require('../middlewares/auth.middleware');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 
 router.post('/register', [
@@ -12,5 +12,15 @@ router.post('/register', [
 ],
     userController.registerUser
 )
+
+router.get('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+],
+    userController.loginUser
+)
+
+router.get('/profile', authMiddleware.authUser, userController.userProfile)
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 module.exports = router
